aab7b53d135998bd1029d4d85827443c23ea158c
Alex Pushkin <pushkin567567@gmail.com>
2024-12-12 19:57:58 +0300

    First commit

---
+++ app/main.py
@@ -0,0 +1,89 @@
+import time
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from redis import asyncio as aioredis
+from sqladmin import Admin
+from app.logger import logger
+from app.admin.views import BookingsAdmin, HotelsAdmin, RoomsAdmin, UsersAdmin
+from app.bookings.router import router as router_bookings
+from app.config import settings
+from app.database import engine
+from fastapi_versioning import VersionedFastAPI
+from app.hotels.router import router as router_hotels
+from app.images.router import router as router_images
+from app.pages.router import router as router_pages
+from app.users.router import router_auth, router_users
+from prometheus_fastapi_instrumentator import Instrumentator, metrics
+from app.prometheus.router import router as router_prometheus
+import sentry_sdk
+# from app.admin.auth import authentication_backend
+app = FastAPI()
+
+if settings.MODE != "TEST":
+    # Подключение Sentry для мониторинга ошибок. Лучше выключать на период локального тестирования
+    sentry_sdk.init(
+        dsn=settings.SENTRY_DSN,
+        traces_sample_rate=1.0,
+    )
+
+
+app.include_router(router_auth)
+app.include_router(router_users)
+app.include_router(router_hotels)
+app.include_router(router_bookings)
+app.include_router(router_pages)
+app.include_router(router_images)
+app.include_router(router_prometheus)
+
+app = VersionedFastAPI(app,
+                       version_format='{major}',
+                       prefix_format='/v{major}',
+                       )
+
+if settings.MODE == "TEST":
+    # При тестировании через pytest, необходимо подключать Redis, чтобы кэширование работало.
+    # Иначе декоратор @cache из библиотеки fastapi-cache ломает выполнение кэшируемых эндпоинтов.
+    # Из этого следует вывод, что сторонние решения порой ломают наш код, и это бывает проблематично поправить.
+    redis = aioredis.from_url(
+        f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}", encoding="utf8", decode_responses=True)
+    FastAPICache.init(RedisBackend(redis), prefix="cache")
+
+
+@app.on_event("startup")
+def startup():
+    redis = aioredis.from_url(
+        f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}", encoding="utf8", decode_responses=True)
+    FastAPICache.init(RedisBackend(redis), prefix="cache")
+
+
+instrumentator = Instrumentator(
+    should_group_status_codes=False,
+    excluded_handlers=[".*admin.*", "/metrics"],
+)
+
+instrumentator.instrument(app).expose(app)
+
+admin = Admin(app, engine)  # , authentication_backend=authentication_backend)
+
+
+admin.add_view(UsersAdmin)
+admin.add_view(HotelsAdmin)
+admin.add_view(RoomsAdmin)
+admin.add_view(BookingsAdmin)
+
+
+app.mount("/static", StaticFiles(directory="app/static"), "static")
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+#     # При подключении Prometheus + Grafana подобный лог не требуется
+#     logger.info("Request handling time", extra={
+#         "process_time": round(process_time, 4)
+#     })
+    return response
